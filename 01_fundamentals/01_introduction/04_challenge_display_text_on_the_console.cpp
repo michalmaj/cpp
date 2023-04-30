9d56e5347583b209a8fca4a28b9d419ca7fe964e
mmaj <michalpmaj@gmail.com>
2023-04-30 10:29:45 +0200

    Challenge: Display Text on the Console

---
+++ 01_fundamentals/01_introduction/04_challenge_display_text_on_the_console.cpp
@@ -0,0 +1,12 @@
+/*
+ * In this challenge your task is to display the following text on the console.
+ * Hey! I am John.
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hey! I am John." << std::endl;
+    
+    return 0;
+}
