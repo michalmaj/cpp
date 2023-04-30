0b2c13f4e2ffe212dbba6900912327e7c426ef87
mmaj <michalpmaj@gmail.com>
2023-04-30 10:37:52 +0200

    Challenge: Display a Right-Angle Triangle

---
+++ 01_fundamentals/01_introduction/05_challenge_display_a_right_angle_triangle.cpp
@@ -0,0 +1,22 @@
+/*
+ * Your task is to display the following patter on the screen:
+ *  * * * * * *
+ *  * * * * *
+ *  * * * *
+ *  * * * 
+ *  * *
+ *  *
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "* * * * * *" << std::endl;
+    std::cout << "* * * * *" << std::endl;
+    std::cout << "* * * *" << std::endl;
+    std::cout << "* * *" << std::endl;
+    std::cout << "* *" << std::endl;
+    std::cout << "*" << std::endl;
+
+    return 0;
+}
