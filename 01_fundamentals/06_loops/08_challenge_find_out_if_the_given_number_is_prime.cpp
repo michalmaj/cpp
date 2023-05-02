c3198600085265b2e95d512c30d3ac3645d6fb5e
mmaj <michalpmaj@gmail.com>
2023-05-02 15:48:03 +0200

    Challenge: Find out if the Given Number is Prime

---
+++ 01_fundamentals/06_loops/08_challenge_find_out_if_the_given_number_is_prime.cpp
@@ -0,0 +1,31 @@
+/*
+ * Read number from keyboard and find whether the given number is prime or not.
+*/
+
+#include <iostream>
+
+int main(){
+    // Create bool varaible to store information f variable is prime or not
+    bool is_prime = true;
+    // Declare variable to store a namber
+    int number;
+    std::cout << "Enter number to check if it's a prime number: ";
+    std::cin >> number;
+    
+    if(number < 2)
+        is_prime = false;
+    
+    for(int i = 2; i < number / 2 + 1; ++i){
+        if (number == 2)
+            break;
+        if (number % i == 0){
+            is_prime = false;
+            break;
+        }
+    }
+    
+    std::cout << ((is_prime)? "prime" : "not prime") << "\n";
+
+
+    return 0;
+}
