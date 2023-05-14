d36a33ea93af883f4af86d9cfbab9fafcbd0f9b7
mmaj <michalpmaj@gmail.com>
2023-05-14 21:40:03 +0200

    Smart Pointers Exercise

--- 02_arrays_and_memory_allocation/04_smart_pointers/09_smart_pointers_exercise.cpp
+++ 02_arrays_and_memory_allocation/04_smart_pointers/09_smart_pointers_exercise.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <memory>
 
+// With the class std::enable_shared_from_this, we can create objects which return an std::shared_ptr on
+// itself. For that, we must derive the class public from std::enable_shared_from_this.
 class ShareMe: public std::enable_shared_from_this<ShareMe>{
 public:
     std::shared_ptr<ShareMe> getShared(){
@@ -14,6 +16,9 @@ public:
 int main(){
     //share the same ShareMe object
     auto shareMe1 = std::make_shared<ShareMe>();
+    
+    // The smart pointer shareMe1 is copied by shareMe. The call shareMe->getShared() 
+    // creates new smart pointer. get_shared() internally uses the function shared_from_this.
     auto shareMe2 = shareMe1->getShared();
     
     // Both resource have the same address
