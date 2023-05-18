7802e92f74a4cfc6061894e57b4ae52b898b0bcc
mmaj <michalpmaj@gmail.com>
2023-05-18 20:28:07 +0200

    Class Definition

---
+++ 03_oop_core_concepts/02_classes/01_class_definition.cpp
@@ -0,0 +1,28 @@
+/*
+ * In C++, we have several differnet data types like int, double, bool, char etc. An object can be created
+ * out of any those types.An object is an instance of a class.
+ * Classes are used to creat user-defined data types. We can use these basic data types to create our own
+ * class. The cool part is that our class can contain multiple variables, pointers, and functions which
+ * would be available to us whenever a class object is created.
+ * In general, two categories of members, data and functions, are present in all classes.
+ *
+ * Data members are also known as the member variables of a class. This is because they contain the
+ * information relevant to the object of the class.
+ *
+ * Member functions enable the class object to perform operations using the memebr variables.
+ *
+ * The reserved word class tells the compiler that we are creating our own custom class. All the members of
+ * the class will be defined within the class scope.  
+*/
+
+class className{
+    /* All member variables
+    and member functions */
+};
+
+int main(){
+    int i; // Integer object
+    className c; // className object
+
+    return 0;
+}
