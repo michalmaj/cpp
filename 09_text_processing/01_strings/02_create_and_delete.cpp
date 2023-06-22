d1771f3cfeb93b116ffd3d749911b9270992d34a
mmaj <michalpmaj@gmail.com>
2023-06-22 20:47:46 +0200

    Create and Delete

---
+++ 09_text_processing/01_strings/02_create_and_delete.cpp
@@ -0,0 +1,71 @@
+/*
+ * C++ offers many methods to create strings from C or C++ strings. Under the hood, there is always a C 
+ * string involved for creating a C++ string. That changes with C++14, because the new C++ standard supports
+ * C++ string literals: std::string str{"string"'s}. With the suffix s, the C string literal "string 
+ * literal" becomes a C++ string literal: "string literal"s.
+ *
+ * Overview of the methods to create and delete a C++ string:
+ *  Method                                          Example
+ *  Default                                         std::string str
+ *  Copies from a C++ string                        std::string str(oth)
+ *  Moves from a C++ string                         std::string(std::move(oth))
+ *  From the range of a C++ string                  std::string(oth.begin(), oth.end())
+ *  From a substring of a C++ string                std::string(oth, otherIndex)
+ *  From a substring of a C++ string                std::string(oth, otherIndex, strlen) 
+ *  From a C string                                 std::string str("c-string")
+ *  From a C array                                  std::string str("c-array", len)
+ *  From characters                                 std::string str(num, 'c')
+ *  From a initializer list                         std::string str({'a', 'b', 'c', 'd'})
+ *  From a substring                                str = other.substring(3, 10)
+ *  Destructor                                      str.~string()
+*/
+#include <iostream>
+#include <string>
+#include <utility>
+
+int main(){
+    std::string defaultString;
+    
+    std::cout << "From C-String\n";
+    std::string other{"123456789"};
+    std::cout << "other: " << other << std::endl;
+    
+    std::cout << std::endl;
+    
+    std::cout << "From C++ string\n";
+    
+    std::string str1(other);
+    std::string tmp(other);
+    std::string str2(std::move(tmp));
+    std::string str3(other.begin(), other.end());
+    std::string str4(other, 2);
+    std::string str5(other, 2, 5);
+    
+    std::cout << "str1: " << str1 << std::endl;
+    std::cout << "str2: " << str2 << std::endl;
+    std::cout << "str3: " << str3 << std::endl;
+    std::cout << "str4: " << str4 << std::endl;
+    std::cout << "str5: " << str5 << std::endl;
+    
+    std::cout << std::endl;
+    
+    std::cout << "From C-String\n";
+    
+    std::string str6("123456789", 5);
+    std::string str7(5, '1');
+    std::string str8({'1', '2', '3', '4', '5', '6', '7', '8', '9'});
+    
+    std::cout << "str6: " << str6 << std::endl;
+    std::cout << "str7: " << str7 << std::endl;
+    std::cout << "str8: " << str8 << std::endl;
+    
+    std::cout << std::endl; 
+    
+    std::cout << "As Part of a C++ String\n";
+    std::cout << "str6.substr(): " << str6.substr() << std::endl;
+    std::cout << "str6.substr(1): " << str6.substr(1) << std::endl;
+    std::cout << "str6.substr(1, 2): " << str6.substr(1, 2) << std::endl;
+
+
+    return 0;
+}
