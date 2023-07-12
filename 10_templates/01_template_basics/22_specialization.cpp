b768aef3763604baf407eb723f55fc7ff9d06379
mmaj <michalpmaj@gmail.com>
2023-07-12 21:21:45 +0200

    Specialization: Template Types

---
+++ 10_templates/01_template_basics/22_specialization.cpp
@@ -0,0 +1,24 @@
+/*
+ * Template Types
+*/
+#include <iostream>
+#include <string>
+
+template <typename T>
+struct Type{
+  std::string getName() const{
+    return "unknown";
+  }
+};
+
+int main(){
+  std::cout << std::boolalpha << std::endl;
+
+  Type<float> tFloat;
+  std::cout << "tFloat.getName(): " << tFloat.getName() << std::endl;
+
+  std::cout << std::endl;
+
+
+  return 0;
+}
