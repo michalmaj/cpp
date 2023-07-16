d3e01fb5c416e998b9685658edc88729dd38f375
mmaj <michalpmaj@gmail.com>
2023-07-16 21:37:37 +0200

    Friends

---
+++ 10_templates/02_template_details/11_friends.cpp
@@ -0,0 +1,29 @@
+/*
+ * Class Template Type Friend
+*/
+#include <iostream>
+
+template <typename T>
+class Bank{
+  std::string secret{"Import secret from the bank."};
+  friend T;
+};
+
+class Account{
+public:
+  Account(){
+    Bank<Account> bank;
+    std::cout << bank.secret << std::endl;
+  }
+};
+
+
+int main(){
+  std::cout << std::endl;
+
+  Account acc;
+
+  std::cout << std::endl;
+
+  return 0;
+}
