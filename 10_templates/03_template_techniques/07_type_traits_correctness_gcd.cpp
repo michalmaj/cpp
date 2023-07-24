4f4f32f8b472a4650f1731260b618245cb5c60dd
mmaj <michalpmaj@gmail.com>
2023-07-24 19:53:17 +0200

    Type-Traits Correctness GCD

--- 10_templates/03_template_techniques/07_type_traits_correctness_gcd.cpp
+++ 10_templates/03_template_techniques/07_type_traits_correctness_gcd.cpp
@@ -12,7 +12,7 @@
 // This function has two serious issues:
 // 1. It is too generic. The function template accepts doubles and strings. But it makes no sense to 
 //    determine the gcd of both data types.
-// 2. We depend on one type parameter T. There in no conversion for type parameters. Theresore, the 
+// 2. We depend on one type parameter T. There in no conversion for type parameters. therefore, the 
 //    instantiation of gcd with an int and long type fails.
 template <typename T>
 T gcd1(T a, T b){
@@ -42,11 +42,11 @@ T gcd2(T a, T b){
 // std::conditional<(sizeof(T1) < sizeof(T2)), T1, T2>::type will return, at compile-time, T1 if T1 is 
 // smaller than T2; it will return T2 if T2 is not smaller than T1.
 template <typename T1, typename T2>
-typename std::conditional<(sizeof(T1) < sizeof(T2)), T1, T2>::type gcd3(T1 a, T2 b){
+typename std::conditional<(sizeof(T1) < sizeof(T2)), T1, T2>::type gcd3a(T1 a, T2 b){
   static_assert(std::is_integral<T1>::value, "T1 should be integral!\n");
   static_assert(std::is_integral<T2>::value, "T2 should be integral!\n");
   if(b == 0) return a;
-  else return gcd3(b, a & b);
+  else return gcd3a(b, a & b);
 }
 
 // gcd - The Third (The Common Type)
@@ -57,15 +57,37 @@ typename std::conditional<(sizeof(T1) < sizeof(T2)), T1, T2>::type gcd3(T1 a, T2
 // return type. With the argument types int and int we get int; with the argument types int and long int
 // we get long int, and with int and long long int we get long long int.
 template <typename T1, typename T2>
-typename std::common_type<T1, T2>::type gcd4(T1 a, T2 b){
+typename std::common_type<T1, T2>::type gcd3b(T1 a, T2 b){
   static_assert(std::is_integral<T1>::value, "T1 should be an integral type!\n");
   static_assert(std::is_integral<T2>::value, "T2 should be an integral type!\n");
+  if(b == 0) return a;
+  else return gcd3b(b, a % b);
+}
+
+// gcd - The Fourth
+// std::enable_if from the type-traits library provides a very interesting variation. What the previous 
+// implementations have in common is that they will check in the function body if the arguments are of 
+// the integral types or not. The key observation is that the compiler always tries to instantiate the 
+// function templates but sometimes fals. We know the result. If the expression std::is_integral returns
+// false, the instantiation will fail. That is not the best way. It would be better if the functon 
+// template is only available for the valid types. Therefore, we put the check of the function template 
+// from the template body to the template signature.
+// The expression std::is_integral determines whether the type parameter T1 and T2 are integrals. If T1 
+// and T2 are not integrals, and therefore they return false, we will not get a template instantiation.
+// If std::enable_if returns true as the first parameter, std::enable_if will check a public memeber
+// typedef type. If std::enable_if returns false as first parameter, std::enable_if will have no memeber
+// type. This is not an error but a common technique in C++ SFINAE. SFINAE stands for Substitution Is 
+// Not An Error. Only the template for exactly this type will not be instantiated and the compiler tries
+// to instantiate the template in another way.
+template <typename T1, typename T2,
+  typename std::enable_if<std::is_integral<T1>::value, T1>::type = 0,
+  typename std::enable_if<std::is_integral<T2>::value, T2>::type = 0,
+  typename R = typename std::conditional<(sizeof(T1) < sizeof(T2)), T1, T2>::type>
+R gcd4(T1 a, T2 b){
   if(b == 0) return a;
   else return gcd4(b, a % b);
 }
 
-// gcd - The Fourth
-// std::enable_if from 
 
 int main(){
   std::cout << "gcd - The First:\n";
@@ -85,18 +107,25 @@ int main(){
   std::cout << std::endl;
 
   std::cout << "gcd - The Third (The Smaller Type):\n";
-  std::cout << gcd3(100,10LL) << std::endl;
-  auto res = gcd3(100,10LL);
-  std::conditional<(sizeof(long long)<sizeof(long)), long long, long>::type res2=gcd3(100LL,10L);
+  std::cout << gcd3a(100,10LL) << std::endl;
+  auto res = gcd3a(100,10LL);
+  std::conditional<(sizeof(long long)<sizeof(long)), long long, long>::type res2=gcd3a(100LL,10L);
   std::cout << typeid(res).name() << std::endl;  // i 
   std::cout << typeid(res2).name() << std::endl; // l 
 
   std::cout << std::endl;
   
   std::cout << "gcd - The Third (The Smaller Type):\n";
-  std::cout << typeid(gcd4(100, 10)).name() << std::endl;  // i 
-  std::cout << typeid(gcd4(100, 10L)).name() << std::endl;  // l 
-  std::cout << typeid(gcd4(100, 10LL)).name() << std::endl;  // x
+  std::cout << typeid(gcd3b(100, 10)).name() << std::endl;  // i 
+  std::cout << typeid(gcd3b(100, 10L)).name() << std::endl;  // l 
+  std::cout << typeid(gcd3b(100, 10LL)).name() << std::endl;  // x
+  
+  std::cout << std::endl;
+  
+  std::cout << "gcd - The Fourth:\n";
+  std::cout << "gcd4(100, 10)= " << gcd4(100, 10) << std::endl; 
+  std::cout << "gcd4(100, 33)= " << gcd4(100, 33) << std::endl; 
+  //std::cout << "gcd4(3.5, 4.0)= " << gcd4(3.5, 4.0) << std::endl;
 
 
   return 0;
