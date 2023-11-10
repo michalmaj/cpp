6b6535fa92c125410abfc8e211bb121bbad9ccca
Michał Maj <michalpmaj@gmail.com>
2023-11-10 19:48:29 +0100

    Function Templates: Exercise

--- 10_templates/01_template_basics/03_function_templates_exercise.cpp
+++ 10_templates/01_template_basics/03_function_templates_exercise.cpp
@@ -1,58 +1,53 @@
 /*
- * We have to implement a function which calculates 2^10 in the program. Try using templates to implement
- * the function.
-*/
+ * Problem statement:
+ * We have to implement a function which calculates 2^10 in the program.
+ */
+
 #include <iostream>
 
 // Solution 1: Using Function Arguments
-// We're using a for loop to compute the power. The loop runs a total of n times by multiplying the
-// number m with r for every iteration of the loop.
-int power1(int m, int n){
-  int r = 1;
-  for(int k=1; k<=n; ++k) 
-      r *= m;
-  return r;
+// We're using for loop to compute the power, The loop runs a total of p-1 times by multiplying the number res with n
+// for every iteration.
+int power_args(int n, int p){
+    int res = 1;
+    for(int i{0}; i < p; ++i)
+        res *= n;
+
+    return res;
 }
 
 // Solution 2: Using Template Arguments
-// First, the primary template is called. Then the Power2<m, n-1>::value executed.
-// This expression instantiates recursively until the end condtion is met (n is equal to 0).
-template <int m, int n>
-struct Power2{
-  static int const value = m * Power2<m, n-1>::value;
+// First, this template (primary) is called, then the PowerTemplateArgs<n, p-1>::value is executed. This expression
+// instantiates recursively until the end condition is met: p is equal to 0. Now, the boundary condition is applied,
+// which returns 1.
+template <int n, int p>
+struct PowerTemplateArgs{
+    static const int value = n * PowerTemplateArgs<n, p-1>::value;
 };
-// Recursion base case
-template <int m>
-struct Power2<m, 0>{ 
-  static int const value = 1;
-};
+ template<int n>
+ struct PowerTemplateArgs<n, 0>{
+     static const int value = 1;
+ };
 
-// Solution 3: Using Template Arguments and Function Arguments
-// In this case the power function template exists in three variations. First the primary template below.
-// Second and third in the full specializations for 1 and 0 are below.
-template <int n>
-int power3(int m){
-  return m * power3<n-1>(m);
-}
+ // Solution 3: Using Template Arguments and Function Arguments
+ template<int p>
+ int powerTemplateFunctionArgs(int n){
+     return n * powerTemplateFunctionArgs<p-1>(n);
+ }
+ template<>
+ int powerTemplateFunctionArgs<1>(int n){
+     return n;
+ }
 
-template<>
-int power3<1>(int m){
-  return m;
-}
+int main(){
+    // First solution
+    std::cout << "Solution 1: Using Function Arguments: " << power_args(2, 10) << std::endl;
 
-template<>
-int power3<0>(int m){
-  return 1;
-}
+    // Second solution, the call PowerTemplateArgs<2, 10>::value triggers recursive calculation.
+    std::cout << "Solution 2: Using Template Arguments: " << PowerTemplateArgs<2, 10>::value << std::endl;
 
+    // Third solution
+    std::cout << "Solution 3: Using Template Arguments and Function Arguments: " << powerTemplateFunctionArgs<10>(2);
 
-
-int main() {
-  std::cout << "Solution1: " << power1(2, 10) << std::endl;
-  
-  // The call Power2<2, 10>::value triggers the recursive calculation.
-  std::cout << "Solution2: " << Power2<2, 10>::value << std::endl;
-
-  // The call power3<10>(2) triggers the recursive invocation of the primary template.
-  std::cout << "Solution3: " << power3<10>(2) <<std::endl;
+    return 0;
 }
