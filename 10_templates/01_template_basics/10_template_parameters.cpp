d64ac35b4c1a934163054a711efd05d8ab02b632
Michał Maj <michalpmaj@gmail.com>
2023-11-15 16:03:52 +0100

    Template Parameters: Template-Template Parameter

--- 10_templates/01_template_basics/10_template_parameters.cpp
+++ 10_templates/01_template_basics/10_template_parameters.cpp
@@ -1,41 +1,45 @@
-/*
- * Template-Template Parameter
-*/
+// Template-Template Parameter
 #include <initializer_list>
 #include <iostream>
 #include <list>
 #include <vector>
 
-template <typename T, template <typename, typename> class Cont>
+template <typename T, template<typename, typename> class Cont>
 class Matrix{
 public:
-  explicit Matrix(std::initializer_list<T> inList): data(inList){
-    std::cout << "Matrix contans: ";
-    for(auto& d : data) std::cout << d << " ";
-    std::cout << std::endl;
-  }
-  int getSize() const{
-    return data.size();
-  }
+    explicit Matrix(std::initializer_list<T> inList): data(inList){
+        for(const auto& d : data)
+            std::cout << d << " ";
+    }
+
+    int getSize() const{
+        return data.size();
+    }
 
 private:
-  Cont<T, std::allocator<T>> data;
+    Cont<T, std::allocator<T>> data;
 };
 
 int main(){
+    std::cout << std::endl;
 
-  Matrix<int, std::vector> myIntVec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  std::cout << "myIntVec.getSize(): " << myIntVec.getSize() << std::endl;
+    Matrix<int, std::vector> myIntVec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << std::endl;
+    std::cout << "myIntVec.getSize(): " << myIntVec.getSize() << std::endl;
 
-  std::cout << std::endl;
+    std::cout << std::endl;
 
-  Matrix<double,std::vector> myDoubleVec{1.1, 2.2, 3.3, 4.4, 5.5};
-  std::cout << "myDoubleVec.getSize(): "  << myDoubleVec.getSize() << std::endl;
+    Matrix<double,std::vector> myDoubleVec{1.1, 2.2, 3.3, 4.4, 5.5};
+    std::cout << std::endl;
+    std::cout << "myDoubleVec.getSize(): "  << myDoubleVec.getSize() << std::endl;
 
-  std::cout << std::endl;
+    std::cout << std::endl;
 
-  Matrix<std::string,std::list> myStringList{"one", "two", "three", "four"};
-  std::cout << "myStringList.getSize(): " << myStringList.getSize() << std::endl;
+    Matrix<std::string,std::list> myStringList{"one", "two", "three", "four"};
+    std::cout << std::endl;
+    std::cout << "myStringList.getSize(): " << myStringList.getSize() << std::endl;
 
-  return 0;
+    std::cout << std::endl;
+
+    return 0;
 }
