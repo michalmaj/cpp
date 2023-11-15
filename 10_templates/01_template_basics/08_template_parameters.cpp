38563ff2e247d1aab4747f293fdf633aee612ad9
Michał Maj <michalpmaj@gmail.com>
2023-11-15 15:43:27 +0100

    Template Parameters: Type Parameter

--- 10_templates/01_template_basics/08_template_parameters.cpp
+++ 10_templates/01_template_basics/08_template_parameters.cpp
@@ -1,64 +1,64 @@
 /*
- * Every template is parametrized by one or more template parameters, indicated in the parameter-list of
- * the template.
- * C++ supports three different kinds of templates parameters.
+ * Template Parameters
+ * Every template is parametrized by one or more template parameters, indicated in the parameter-list of the template.
+ * C++ supports three different kinds of template parameters:
  *  1. Type parameter:
- *    std::vector<int> vec {1, 2, 3, 4, 5};
+ *      std::vector<int> vec = {1, 2, 3, 4, 5};
  *  2. Non-type parameter
- *    std::array<int, 5> arr{1, 2, 3, 4, 5};
+ *      std::array<int, 5> arr = {1, 2, 3, 4, 5};
  *  3. Template-template parameter
- *    template <typename T, template <typename, typename> class Cont>
- *    class Matrix{...};
- *
- *    Matrix<int, std::vector> myntVec;
+ *      template <typename T, template <typename, typename> class Cont> class Matrix{
+ *      ...
+ *      Matrix<int, std::vector> myIntVec;
  *
  * Types
- * A type parameter is a typical case for template arguments. Type parameters are class types and 
- * fundamental types.
+ * A type parameter is a typical case for template arguments.
+ * Type parameters are class types and fundamental types.
  *
- * Non-Types
- * Non-types are template parameters which can be evaluated at compile time. The following types are 
- * possible:
+ * Non-types
+ * Non-types are template parameters which can be evaluated at compile-time.
+ * The following types are possible:
  *  - Integers and enumerations.
- *  - Pointers to objects, functions, and attributes of a class.
- *  - References to objects and functions.
+ *  - Pointers to object, functions, and attributes of a class.
+ *  - References to object and functions.
  *  - std::nullptr_t constant.
- * Note: Wth C++17, floating-point numbers and strings cannot be used as non-type parameters.
-*/
+ * Note: with C++17, floating-point numbers and strings cannot be used as non-type parameters.
+ */
+// Type Parameter
 #include <iostream>
 #include <typeinfo>
 
 class Account{
 public:
-  explicit Account(double amt): balance(amt) {}
-
+    explicit Account(double amt): balance(amt) {}
 private:
-  double balance;
+    double balance;
 };
 
 union WithString{
-  std::string s;
-  int i;
-  WithString(): s("hello") {}
-  ~WithString() {}
+    std::string s;
+    int i;
+    WithString(): s("hello") {}
+    ~WithString() = default;
 };
 
 template <typename T>
 class ClassTemplate{
 public:
-  ClassTemplate(){
-    std::cout << "typeid(T).name(): " << typeid(T).name() << std::endl;
-  }
+    ClassTemplate(){
+        std::cout << "typeid(T).name(): " << typeid(T).name() << std::endl;
+    }
 };
 
 int main(){
-  ClassTemplate<int> clTempInt;
-  ClassTemplate<double> clTempDouble;
-  ClassTemplate<std::string> clTempString;
+    std::cout << std::endl;
 
-  ClassTemplate<Account> clTempAccount;
-  ClassTemplate<WithString> clTempWithString;
+    ClassTemplate<int> clTempInt;
+    ClassTemplate<double> clTempDouble;
+    ClassTemplate<std::string> clTempString;
 
+    ClassTemplate<Account> clTempAccount;
+    ClassTemplate<WithString> clTempWithString;
 
-  return 0;
+    return 0;
 }
