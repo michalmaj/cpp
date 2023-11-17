8cf75080356c7aef1c0488d972b709d98eff32b1
Michał Maj <michalpmaj@gmail.com>
2023-11-17 10:26:41 +0100

    Template Parameters: Exercise

--- 10_templates/01_template_basics/11_template_parameters_exercise.cpp
+++ 10_templates/01_template_basics/11_template_parameters_exercise.cpp
@@ -1,54 +1,62 @@
 /*
- * Implement four different class templates which accept an integral, a pointer, a reference, or a nullptr
- * as a non-type parameter. You need to display the type of data passed as a parameter.
-*/
-
-#include <iostream>
+ * Problem statement
+ * Implement four different class template which accept an integral, a pointer, a reference, or a nullptr as non-type
+ * parameter. YOu need to display the type of data passed as a parameter.
+ */
 #include <cstddef>
+#include <iostream>
 #include <typeinfo>
 
 // Write class templates for given commented code
+
+// AcceptChar<'c'> acceptChar
 template <char c>
 class AcceptChar{
 public:
-  AcceptChar(){
-    std::cout << "AcceptChar: " << typeid(c).name() << std::endl;
-  }
+    AcceptChar(){
+        std::cout << "AcceptChar: " << typeid(c).name() << std::endl;
+    }
 };
 
-template <int(*func)(int)>
+// AcceptFunction< myFunc> acceptFunction
+template <int (*func)(int)>
 class AcceptFunction{
 public:
-  AcceptFunction(){
-    std::cout << "AcceptFunction: " << typeid(func).name() << std::endl;
-  }
+    AcceptFunction(){
+        std::cout << "AcceptFunction: " << typeid(func).name() << std::endl;
+    }
 };
 
-template <int(&arr)[5]>
+// AcceptReference< arr > acceptReference
+template <int (&arr)[5]>
 class AcceptReference{
 public:
-  AcceptReference(){
-    std::cout << "AcceptReference: " << typeid(arr).name() << std::endl;
-  }
+    AcceptReference(){
+        std::cout << "AcceptReference: " << typeid(arr).name() << std::endl;
+    }
 };
 
+// AcceptNullptr< nullptr > acceptNullptr
 template <std::nullptr_t N>
 class AcceptNullptr{
 public:
-  AcceptNullptr(){
-    std::cout << "AcceptNullptr: " << typeid(N).name() << std::endl;
-  }
+    AcceptNullptr(){
+        std::cout << "AcceptNullptr: " << typeid(N).name() << std::endl;
+    }
 };
 
 int myFunc(int){return 2023;}
-int arr[5];
+int arr[5]{};
+
 
 int main() {
-  // uncomment these lines after implementing codes
 
-  
-  AcceptChar<'c'> acceptChar;
-  AcceptFunction< myFunc> acceptFunction;
-  AcceptReference< arr > acceptReference;
-  AcceptNullptr< nullptr > acceptNullptr;
+    std::cout << std::endl;
+
+    AcceptChar<'c'> acceptChar;
+    AcceptFunction<myFunc> acceptFunction;
+    AcceptReference<arr> acceptReference;
+    AcceptNullptr<nullptr> acceptNullptr;
+
+    return 0;
 }
